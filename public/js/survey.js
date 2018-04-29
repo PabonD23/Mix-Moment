81d927cf9dc4a319dfcb948429641de75a3321e1
Shannon Doepke <shannondoepke@Shannons-Air.lan>
2018-04-29 16:39:54 -0400

    Update check boxes and function

--- public/js/survey.js
+++ public/js/survey.js
@@ -37,71 +37,6 @@ $(document).ready(function() {
   
   });
 
-  //Controls to add red borders to selected photos
-$("#int-link-1").on("click", function(){
-  $("#int-image1").attr("class", "img-active");
-});
-
-$("#int-link-2").on("click", function(){
-  $("#int-image2").attr("class", "img-active");
-});
-
-$("#int-link-3").on("click", function(){
-  $("#int-image3").attr("class", "img-active");
-});
-
-$("#int-link-4").on("click", function(){
-  $("#int-image4").attr("class", "img-active");
-});
-
-$("#int-link-5").on("click", function(){
-  $("#int-image5").attr("class", "img-active");
-});
-
-$("#int-link-6").on("click", function(){
-  $("#int-image6").attr("class", "img-active");
-});
-
-$("#int-link-7").on("click", function(){
-  $("#int-image7").attr("class", "img-active");
-});
-
-$("#int-link-8").on("click", function(){
-  $("#int-image8").attr("class", "img-active");
-});
-
-$("#int-link-9").on("click", function(){
-  $("#int-image9").attr("class", "img-active");
-});
-
-$("#int-link-10").on("click", function(){
-  $("#int-image10").attr("class", "img-active");
-});
-
-$("#int-link-11").on("click", function(){
-  $("#int-image11").attr("class", "img-active");
-});
-
-$("#int-link-12").on("click", function(){
-  $("#int-image12").attr("class", "img-active");
-});
-
-$("#int-link-13").on("click", function(){
-  $("#int-image13").attr("class", "img-active");
-});
-
-$("#int-link-14").on("click", function(){
-  $("#int-image14").attr("class", "img-active");
-});
-
-$("#int-link-15").on("click", function(){
-  $("#int-image15").attr("class", "img-active");
-});
-
-$("#int-link-16").on("click", function(){
-  $("#int-image16").attr("class", "img-active");
-});
-
 $(document).ready(function() {
 // Getting a reference to the input field where user adds information
 var $nameInput = $("#nameInput");
@@ -109,6 +44,8 @@ var $locationInput = $("#locationInput");
 var $usernameInput = $("#usernameInput");
 var $passwordInput = $("#passwordInput");
 var $emailInput = $("#emailInput");
+
+//Reference to interests
 var $outdoor = $("#int-link-1");
 var $fitness = $("#int-link-2");
 var $photography = $("#int-link-3");
@@ -129,6 +66,7 @@ var $career = $("#int-link-16");
 //   Click event for submit button
   $("#submit-survey").on("click", function(){
     addNewUser();
+    addNewInterests();
 
   })
 
@@ -145,11 +83,28 @@ var $career = $("#int-link-16");
       $.post("/api/users", newUser);
       };
 
-  function addInterests (event) {
-    var userInterests = {
+  function addNewInterests () {
+    var newInterest = {
       username: $usernameInput.val().trim(),
-      
+      outdoor: $outdoor.val(),
+      fitness: $fitness.val(),
+      photography: $photography.val(),
+      dance: $dance.val(),
+      culture: $culture.val(),
+      technology: $technology.val(),
+      gaming: $gaming.val(),
+      travel: $travel.val(),
+      food: $food.val(),
+      books: $books.val(),
+      fashion: $fashion.val(),
+      family: $family.val(),
+      social: $social.val(),
+      music: $music.val(),
+      pets: $pets.val(),
+      career: $career.val()
+  };
 
-    }
+  $.post("/api/interests", newInterest);
   }
+
 });
