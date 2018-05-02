4a2ea35cb580a744475c8b1cd27dc4d6bfaf2ea1
Shannon Doepke <shannondoepke@Shannons-Air.lan>
2018-05-01 20:34:39 -0400

    Update meetups page

--- models/meetups.js
+++ models/meetups.js
@@ -4,11 +4,19 @@ module.exports = function(sequelize, DataTypes) {
         type: DataTypes.DATE,
         allowNull: false,
       },
+      time: {
+        type: DataTypes.TIME,
+        allowNull: false,
+      },
       title: {
         type: DataTypes.STRING,
         allowNull: false,
       },
       category: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      address: {
         type: DataTypes.STRING,
         allowNull: true,
       },
